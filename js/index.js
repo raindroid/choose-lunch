d9a50bb6d4742273011ac5e1c34b8f3f17e38241
rain laptop <yucan.wu@outlook.com>
2019-05-13 15:55:27 -0400

    version 0.0.1

---
+++ js/index.js
@@ -0,0 +1,50 @@
+'use strict'
+
+let rests = [
+        '食来食往',
+        '福稻米线',
+        '绿茶',
+        '西域食府',
+        '刘公烤鱼',
+        '富来小厨',
+        '龙升栈',
+        'stay',
+        '黄记煌',
+        '98香',
+        '满庭芳',
+        '马路边边',
+        '味千拉面',
+        'kington',
+        '马路边边',
+        '嘿小面']
+
+let text_colors = [
+    '#E16B8C',
+    '#F596AA',
+    '#D7C4BB',
+    '#FB966E',
+    '#E2943B',
+    '#FFC408',
+    '#B1B479',
+    '#227D51',
+    '#3A8FB7'
+]
+
+function onStart() {
+
+    findNewFood()
+    
+}
+
+function findNewFood() {
+
+    let choice = Math.floor(Math.random() * rests.length)
+    let today = rests[choice]
+    let content = document.getElementById('content')
+    let ref_btn = document.getElementById('refresh-btn')
+    
+    content.innerHTML = today
+    content.style.color = text_colors[Math.floor(Math.random() * text_colors.length)]
+    ref_btn.style.background = text_colors[Math.floor(Math.random() * text_colors.length)]
+    ref_btn.style.width = today.length * 50 + 'px'
+}
